3ed6171cfb6748afb75926828fb325b773703b74
nick bilayev <kolakolasic@nicks-MacBook-Pro.local>
2018-03-11 16:06:43 +0200

    First commit

---
+++ get_reviews/data.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import urllib.request
+
+
+def get_html_content(name_of_product):
+    """ str -> BeautifulSoap """
+    url = "https://rozetka.com.ua/ua/search/?text=" + name_of_product
+    with urllib.request.urlopen(url) as response:
+        html = response.read().decode("utf-8")
+        soup = BeautifulSoup(html, "html.parser")
+        new_url = soup.find("div", class_="g-i-tile-i-box-desc").find("a")[
+                      "href"] + "/comments"
+        data = map(lambda x: x.text, get_comments(new_url))
+
+        return data
+
+
+def get_comments(url):
+    """ str -> str """
+    with urllib.request.urlopen(url) as response:
+        soup = BeautifulSoup(response.read().decode("utf-8"), "html.parser")
+
+        return soup.find_all("div", "pp-review-text-i")
---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from get_reviews import data
+from get_reviews.you_tube import *
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def data_reg():
+    return render_template("index.html")
+
+
+@app.route('/', methods=['POST'])
+def data_handle():
+    if request.form['text_field']:
+        print(youtube_search(request.form['text_field']))
+        return render_template("data_handled.html", data=data.get_html_content(request.form['text_field']),
+                               videos=youtube_search(request.form['text_field']))
+
+
+if __name__ == '__main__':
+    app.run()
